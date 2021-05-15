aae30ed090450e89e2d5955f5fc3c75e1ff0ceb0
Zoey Holt <zoeyyholt@gmail.com>
2021-05-14 21:12:57 -0700

    coding problems solution first commit includes problems 1-5 from leetcode

---
+++ CodingProblems/LeetCodeProblems.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingProblems
+{
+    public class LeetCodeProblems
+    {
+        // 1. Two Sum
+        // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        // You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        // You can return the answer in any order.
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var result = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (result.Contains(i))
+                {
+                    continue;
+                }
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    if (result.Contains(j))
+                    {
+                        continue;
+                    }
+                    if (nums[i] + nums[j] == target)
+                    {
+                        result.Add(i);
+                        result.Add(j);
+                    }
+                }
+            }
+            return result.ToArray();
+        }
+
+        // 2. Add Two Numbers
+        // You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+        // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+
+            public ListNode(int[] values)
+            {
+                var start = new ListNode();
+                var node = start;
+                for (int i = 0; i < values.Length; i++)
+                {
+                    int val = values[i];
+                    node.val = val;
+                    if (i < values.Length - 1)
+                    {
+                        node.next = new ListNode();
+                        node = node.next;
+                    }
+                }
+                val = start.val;
+                next = start.next;
+            }
+
+            public int[] ToArray()
+            {
+                var result = new List<int>();
+                var node = this;
+                while (true)
+                {
+                    result.Add(node.val);
+                    node = node.next;
+                    if (node == null) break;
+                }
+                return result.ToArray();
+            }
+        }
+
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {            
+            return AddTwoNumbers(l1, l2, false);
+        }
+
+        private ListNode AddTwoNumbers(ListNode l1, ListNode l2, bool carry)
+        {
+            var sum = (l1?.val ?? 0) + (l2?.val ?? 0) + (carry ? 1 : 0);
+            if (sum > 9)
+            {
+                carry = true;
+                sum -= 10;
+            }
+            else
+            {
+                carry = false;
+            }
+            ListNode next;
+            if (l1?.next != null || l2?.next != null || carry)
+            {
+                next = AddTwoNumbers(l1?.next, l2?.next, carry);
+            }
+            else
+            {
+                next = null;
+            }
+            return new ListNode(sum, next);
+        }
+
+        // 3. Longest Substring Without Repeating Characters
+        // Given a string s, find the length of the longest substring without repeating characters.
+        public int LengthOfLongestSubstring(string s)
+        {
+            var length = 0;
+            var chars = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (chars.Length == 0)
+                {
+                    chars += s[i];
+                    continue;
+                }
+
+                if (chars.Contains(s[i].ToString()))
+                {
+                    length = length > chars.Length ? length : chars.Length;
+                    i -= chars.Length;
+                    chars = "";
+                }
+                else
+                {
+                    chars += s[i];
+                }
+            }
+            length = length > chars.Length ? length : chars.Length;
+            return length;
+        }
+
+        // 4. Median of Two Sorted Arrays
+        // Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+        // The overall run time complexity should be O(log (m+n)).
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            var sum = new int[nums1.Length + nums2.Length];
+            var i = 0;
+            var j = 0;
+            while (i < nums1.Length || j < nums2.Length)
+            {
+                if (i < nums1.Length && j < nums2.Length)
+                {
+                    if (nums1[i] <= nums2[j])
+                    {
+                        sum[i + j] = nums1[i];
+                        i++;
+                    }
+                    else
+                    {
+                        sum[i + j] = nums2[j];
+                        j++;
+                    }
+                }
+                else if (i < nums1.Length)
+                {
+                    sum[i + j] = nums1[i];
+                    i++;
+                }
+                else
+                {
+                    sum[i + j] = nums2[j];
+                    j++;
+                }
+            }
+
+            if (sum.Length == 1)
+                return sum[0];
+
+            var median = (nums1.Length + nums2.Length) / 2;
+            if (median * 2 == nums1.Length + nums2.Length)
+                return (sum[median - 1] + sum[median]) / 2d;
+            return sum[median];
+        }
+
+        // 5. Longest Palindromic Substring
+        // Given a string s, return the longest palindromic substring in s.
+        public string LongestPalindrome(string s)
+        {
+            var longest = s.Substring(0, 1);
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = longest.Length + 1; i + j <= s.Length; j++)
+                {
+                    var test = s.Substring(i, j);
+                    if (IsPalindrome(test) && test.Length > longest.Length)
+                    {
+                        longest = test;
+                    }
+                }
+            }
+            return longest;
+        }
+
+        private bool IsPalindrome(string s)
+        {
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (s[i] != s[s.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Tests/LeedCodeProblemsTests.cs
@@ -0,0 +1,71 @@
+﻿using CodingProblems;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class LeedCodeProblemsTests
+    {
+        [TestCase(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+        [TestCase(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+        [TestCase(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+        [TestCase(new int[] { 0, 1, 2, 3, 4, 5 }, 4, new int[] { 0, 4, 1, 3 })]
+        public void TestTwoSum(int[] nums, int target, int[] expected)
+        {
+            var actual = new LeetCodeProblems().TwoSum(nums, target);
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+
+        [TestCase(new int[] { 2, 4, 3 }, new int[] { 5, 6, 4 }, new int[] { 7, 0, 8 })]
+        [TestCase(new int[] { 0 }, new int[] { 0 }, new int[] { 0 })]
+        [TestCase(new int[] { 9, 9, 9, 9, 9, 9, 9 }, new int[] { 9, 9, 9, 9 }, new int[] { 8, 9, 9, 9, 0, 0, 0, 1 })]
+        [TestCase(new int[] { 9 }, new int[] { 1, 9, 9, 9, 9, 9, 9, 9, 9, 9 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 })]
+        [TestCase(new int[] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, new int[] { 5, 6, 4 }, new int[] { 6, 6, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 })]
+        public void TestAddTwoNumbers(int[] l1, int[] l2, int[] expected)
+        {
+            var actual = new LeetCodeProblems().AddTwoNumbers(new LeetCodeProblems.ListNode(l1), new LeetCodeProblems.ListNode(l2)).ToArray();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase("abcabcbb", 3)]
+        [TestCase("bbbbb", 1)]
+        [TestCase("pwwkew", 3)]
+        [TestCase("", 0)]
+        [TestCase(" ", 1)]
+        [TestCase("au", 2)]
+        [TestCase("dvdf", 3)]
+        public void TestLengthOfLongestSubstring(string s, int expected)
+        {
+            var actual = new LeetCodeProblems().LengthOfLongestSubstring(s);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 1, 3 }, new int[] { 2 }, 2d)]
+        [TestCase(new int[] { 1, 2 }, new int[] { 3, 4 }, 2.5d)]
+        [TestCase(new int[] { 0, 2 }, new int[] { 0, 0 }, 0d)]
+        [TestCase(new int[] { }, new int[] { 1 }, 1d)]
+        [TestCase(new int[] { 2 }, new int[] { }, 2d)]
+        [TestCase(new int[] { 1, 3, 5, 7, 9 }, new int[] { 0, 2, 4, 6, 8 }, 4.5d)]
+        [TestCase(new int[] { 1, 10, 20 }, new int[] { 2, 20, 100 }, 15d)]
+        [TestCase(new int[] { 5, 6, 7, 8 }, new int[] { 1, 2, 3 }, 5d)]
+        [TestCase(new int[] { 1, 8 }, new int[] { 2, 3, 4, 5, 6, 7, 8, 9 }, 5.5d)]
+        public void TestFindMedianSortedArrays(int[] nums1, int[] nums2, double expected)
+        {
+            var actual = new LeetCodeProblems().FindMedianSortedArrays(nums1, nums2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("babad", "bab")]
+        [TestCase("cbbd", "bb")]
+        [TestCase("a", "a")]
+        [TestCase("ac", "a")]
+        [TestCase("racecarsarecool", "racecar")]
+        [TestCase("euazbipzncptldueeuechubrcourfpftcebikrxhybkymimgvldiwqvkszfycvqyvtiwfckexmowcxztkfyzqovbtmzpxojfofbvwnncajvrvdbvjhcrameamcfmcoxryjukhpljwszknhiypvyskmsujkuggpztltpgoczafmfelahqwjbhxtjmebnymdyxoeodqmvkxittxjnlltmoobsgzdfhismogqfpfhvqnxeuosjqqalvwhsidgiavcatjjgeztrjuoixxxoznklcxolgpuktirmduxdywwlbikaqkqajzbsjvdgjcnbtfksqhquiwnwflkldgdrqrnwmshdpykicozfowmumzeuznolmgjlltypyufpzjpuvucmesnnrwppheizkapovoloneaxpfinaontwtdqsdvzmqlgkdxlbeguackbdkftzbnynmcejtwudocemcfnuzbttcoew", "aqkqa")]
+        [TestCase("xdxtfdaarvvznrptwicdldmrmwbdrmyppvamdvofujthfcmkcugvodmlvubgvodectwzparprifwgwfvddlrfdnrpspirtyvxqvbqiglugbmzoyzcfkvbjdrdqqxxzutebgoacczuhopvzjfrnfsylgfgkbmbjqqyggtdjcvjbvpspkjcezanajjzabfidndfdpkuamwvxrbpxzoivlnkwdxedtfnmvicmzebwktpktokibeycbpqzejddwnvimmbzupyxwmrgdbmcujadfexcchdkfvkxsdwkuwuxzhpnjgmqbmidcwywjgcsbydixyxcclcbrzjvrmlrzgmbviifllouykovscaufvxovwmmgubshtoizbwtcpqzwchtkmkjfneuybfglywfrorhmfdgvjdsmegtoytsivnuaceszpfsxgddbweckgziahkslykgdkztmpapnoyawqtyrdcuzaxcohohapektyfbfhrsdnjbgjvwvqpcikdnlkdogsinkfpymkkdburnbksnqfjgjlacqpfqlhsjhhoccdkrjipqwzsxmpjughaqchzlrqkogkryqkuuxhzchovebzgeekuflcgvxugnxcvugqlstmnljlvxonkybmzjmnsvvwfztcplgikptnppbzeygbmdsyimsntveojwsejmastiovbctdkdlfvpyzihhxishtveflnmamlnzqroxknrrkkfpveyzvvasdznykygrpbfkbinrrvheekeumlvlgalqelspvpiydqkwduckimyhpzsxlcpkbvgwmwnasdxuupdhcmxjoushcvcnjyrmuemuydyywpvzhkxsqszaqhnbhjwsokkpployomoawtr", "fwgwf")]
+        public void TestLongestPalindrome(string s, string expected)
+        {
+            var actual = new LeetCodeProblems().LongestPalindrome(s);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
