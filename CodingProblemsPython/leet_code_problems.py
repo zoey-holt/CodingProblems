6bd0359108ad56932be032e64d30fc3ebf3c9f35
Zoey Holt <zoeyyholt@gmail.com>
2021-06-12 19:41:08 -0700

    leetcode problem 1 python faster

--- CodingProblemsPython/leet_code_problems.py
+++ CodingProblemsPython/leet_code_problems.py
@@ -4,13 +4,19 @@ from typing import List
 # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 # You can return the answer in any order.
-# This solution has a time complexity of O(N²) where N = nums.Length.
-# This solution has a space complexity of O(1).
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-    list = []
-    for i in range(0, len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[i] + nums[j] == target:
-                list.append(i)
-                list.append(j)
-    return list
+# This solution has a time complexity of O(N) where N = nums.Length.
+# This solution has a space complexity of O(N) where N = nums.Length.
+def two_sum(nums: List[int], target: int) -> List[int]:
+    d = {}
+    for i, n in enumerate(nums):
+        x = target - n
+        if x in d:
+            return [d[x], i]
+        else:
+            d[n] = i
+    return []
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
--- TestsPython/test_leet_code.py
+++ TestsPython/test_leet_code.py
@@ -1,8 +1,7 @@
 import leet_code_problems as problems
 
 def test_twoSum():
-    assert [0, 1] == problems.twoSum(0, [2, 7, 11, 15], 9)
-    assert [1, 2] == problems.twoSum(0, [3, 2, 4], 6)
-    assert [0, 1] == problems.twoSum(0, [3, 3], 6)
-    assert [0, 4, 1, 3] == problems.twoSum(0, [0, 1, 2, 3, 4, 5], 4)
+    assert [0, 1] == problems.two_sum([2, 7, 11, 15], 9)
+    assert [1, 2] == problems.two_sum([3, 2, 4], 6)
+    assert [0, 1] == problems.two_sum([3, 3], 6)
 
